0dde37ed1ce838704fe59cd18cb37c66c094ffea
Chris Kenis <chris.kenis@KICTS.be>
2018-02-10 12:17:40 +0100

    Update Update-HostFile.ps1
    uses default zip assembly from powershell (.Net 4.5)
    prevents weird returned array of filepath(s) by swapping Join-Path with simpler string concatenation
    complete overwrite of hosts file instead of appending entries to existing file

--- OS/Update-HostFile.ps1
+++ OS/Update-HostFile.ps1
@@ -12,29 +12,33 @@ $ImportHostFilePath,
 
 process{
 if ($DownloadNewVersion){$ImportHostFilePath = Get-NewHostFile}
-else {$ImportHostFilePath = gi $ImportHostFilePath }
-write-Verbose "the new hosts file is: $($ImportHostFilePath.FullName)"
 if ($ImportHostFilePath){
-	$HostEntries = Get-Content $ImportHostFilePath.FullName | Where { $_ -notmatch "\#" } | Where {$_.trim() -ne " " }
+	$ImportHostFilePath = Get-Item $ImportHostFilePath
+	write-Verbose "the import hosts file is: $($ImportHostFilePath.FullName)"
+	$HostEntries = (Get-Content $ImportHostFilePath.FullName) -match $HostsRegex
 	if (-not $HostEntries){Write-warning "No host entries found in importfile, exiting update script...";exit}
-	$Hosts = Get-Content -Path $HostFile.FullName | Where { $_ -notmatch "\#" } | Where {$_.trim() -ne " " }
+	$Hosts = (Get-Content -Path $HostFile.FullName) -match $HostsRegex
 	if ($Hosts){
 		$NewHostEntries = Compare-Object $Hosts $HostEntries -PassThru | ?{$_.sideindicator -eq "=>"} 
-		write-Verbose "there are $($NewHostEntries.count) differences found "
+		write-Verbose "there are $($NewHostEntries.count) new entries found "
 		if ($FilteredSelection){ 
-			$AddHostEntries = $NewHostEntries | Out-GridView -Title "select to be added hostentries" -PassThru
+			$AddHostEntries = $NewHostEntries | Out-GridView -Title "select the host entries to be added" -PassThru
 			write-Verbose "you have selected $($AddHostEntries.Count) entries to add to your hosts file"
 			}
 		else { $AddHostEntries = $NewHostEntries }
 		}
 	else { $AddHostEntries = $HostEntries }
-	if(Get-Confirmation "Do you want to update your hosts file with $($AddHostEntries.count) entries?"){
-		#sort and deduplicate host entries
+	if(Get-Confirmation -prompt "Do you want to update your hosts file with $($AddHostEntries.count) new entries?"){
 		write-Verbose "$($AddHostEntries.Count) entries will be added to $($HostFile.FullName)"
 		Copy-Item $HostFile -Destination "$($HostFile).BAK" -Force
-		Add-Content -path $HostFile.FullName -Value "$($nl)#Update-Host Script dd $(Get-Date)$($nl)" -Encoding UTF8
-		$AddHostEntries | Where {$_.trim() -ne " "} | Sort-Object -Unique | Out-File $HostFile.FullName -Append -Encoding UTF8 -Force
+		$Hosts += "$($nl)#Update-Host Script (https://github.com/chriskenis)"
+		$Hosts += "#$($AddHostEntries.Count) entries added on dd $(Get-Date) $($nl)"
+		#sort and deduplicate host entries
+		$Hosts += $AddHostEntries | Sort-Object -Unique
+		$Hosts | Set-Content $HostFile.FullName -Force
 		}
+	Write-Verbose "clearing DNS cache so the new hosts file is in effect immediately after update"
+	Clear-DnsClientCache
 	}
 else{Write-Error "something went wrong while updating the hosts file"}
 }
@@ -42,7 +46,8 @@ else{Write-Error "something went wrong while updating the hosts file"}
 begin {
 $nl = [Environment]::NewLine
 #debug host file
-$HostFile = gi "$($env:windir)\system32\Drivers\etc\hosts" 
+$HostFile = gi "$($env:windir)\system32\Drivers\etc\hosts"
+$HostsRegex = '^\s*(?<Address>[0-9\.\:]+)\s+(?<Host>[\w\.\-]+)\s*$'
 
 
 Function Get-NewHostFile{
@@ -53,28 +58,33 @@ $DownloadFolder = [Environment]::GetFolderPath("MyDocuments")
 )
 #download zip file and place it in folder under the same name
 $HostFileURL = "$($HostFileURL)/$($HostFileArchive)"
-$DownloadHostFilePath = Join-Path $DownloadFolder $HostFileArchive
+$DownloadHostFilePath = "$DownloadFolder\$HostFileArchive"
 try{
 	(New-Object Net.WebClient).DownloadFile($HostFileURL,$DownloadHostFilePath)
 	#extract zip as new file and overwrite previous download
-	$shell = New-Object -ComObject Shell.Application
-	$zip = $shell.NameSpace($DownloadHostFilePath)
-	$HostFileExtractFolder = Join-Path $DownloadFolder "HostsUpdate"
-	New-Item $HostFileExtractFolder -ItemType Directory
-	foreach ($item in $zip.items()) { $shell.Namespace($HostFileExtractFolder).CopyHere($item)}
-	$HostFilePath = gci $HostFileExtractFolder -File -Filter hosts
-	write-Verbose "found new hosts file: $($HostFilePath.FullName)"
+	$HostFileExtractFolder = "$DownloadFolder\HostsUpdate"
+	New-Item $HostFileExtractFolder -ItemType Directory -ErrorAction SilentlyContinue
+	$NewHostFile = "$HostFileExtractFolder\HOSTS.hosts"
+	#remove file if it already exists, return no error if it doesn't
+	Remove-Item $NewHostFile -ErrorAction SilentlyContinue
+	Add-Type -Assembly 'System.IO.Compression.FileSystem'
+	$zip = [System.IO.Compression.ZipFile]::OpenRead($DownloadHostFilePath)
+	$zip.Entries | where {$_.Name -eq 'HOSTS'} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $NewHostFile, $true)}
+	$zip.Dispose()
+	write-Verbose "found new hosts file: $($NewHostFile)"
 	}
 catch{
 	Write-warning "$($_.Exception)"
-	$HostFilePath = ""
+	$NewHostFile = ""
 	}
-return $HostFilePath
+return $NewHostFile
 }
 
-Function Get-Confirmation ( $question ) {
-$title = 'Update hosts file'
-$prompt = $question
+Function Get-Confirmation {
+param(
+$title = 'Update hosts file',
+$prompt
+)
 $No = New-Object System.Management.Automation.Host.ChoiceDescription '&No','Aborts the update'
 $Yes = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes','Continue'
 $options = [System.Management.Automation.Host.ChoiceDescription[]] ($No,$Yes)
@@ -85,5 +95,5 @@ return $choice
 }
 
 end{
-Clear-DnsClientCache
+
 }
