b645cabc102ecf2cfa78080d4b2f6f85de8e91ca
Chris Kenis <chris.kenis@KICTS.be>
2018-06-09 19:00:27 +0200

    revised v2
    supports pipeline input
    added minutes option

--- FS/New-Almanak.ps1
+++ FS/New-Almanak.ps1
@@ -1,10 +1,8 @@
 [CmdletBinding()]
 param(
 [Parameter(Mandatory = $true, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
-[alias("PSpath", "FullName")]
-[ValidateScript( 
-    {if (-Not ($_ | Test-Path -PathType Container ) ) { throw "Folder does not exist" }
-    return $true })][System.IO.FileInfo]$RootFolder,
+[ValidateScript({ $_ | ForEach-Object {(Get-Item $_).PSIsContainer}})]
+[alias("PSpath", "FullName")][string[]]$RootFolders,
 [Parameter(Mandatory = $false, Position = 1)]
 [datetime]$StartDate = (Get-Date),
 [Parameter(Mandatory=$true, Position=2)]
@@ -15,45 +13,67 @@ param(
 [switch]$Weeks,
 [switch]$Days,
 [switch]$Hours,
+[switch]$Minutes,
+[Parameter(Mandatory=$true)]
 [ValidateSet("FullNames", "Abbreviated", "Numeric")][string]$DateFormat
 )
 
 process {
-for ($RunningDate = $StartDate; $RunningDate -le $EndDate; $Runningdate = $RunningDate.AddDays(1)) {
-    if ($Years.IsPresent) { $script:RunningFolder = New-DateFolder -RunningDate $RunningDate -Path $script:RunningFolder -DateInterval "Year" -DateFormat $YearFormat }
-    if ($Quarters.IsPresent) {
-        switch ($RunningDate.Month){
-            {1..3 -contains $_} { $QuarterNumber = $QuarterLabel -f "1" }
-            {4..6 -contains $_} { $QuarterNumber = $QuarterLabel -f "2" }
-            {7..9 -contains $_} { $QuarterNumber = $QuarterLabel -f "3" }
-            {10..12 -contains $_} { $QuarterNumber = $QuarterLabel -f "4" }
-            }
-            $QuarterFolder = Get-Item -Path $(Join-Path $script:RunningFolder $QuarterNumber) -ErrorAction SilentlyContinue
-            if (-not $QuarterFolder) { $script:RunningFolder = New-Item -Path $(Join-Path $script:RunningFolder $QuarterNumber) -ItemType Directory }
-            else { $script:RunningFolder = $QuarterFolder }
-        }
-    if ($Months.IsPresent) { $script:RunningFolder = New-DateFolder -RunningDate $RunningDate -Path $script:RunningFolder -DateInterval "Month" -DateFormat $MonthFormat }
-    if ($Weeks.IsPresent) {
-        switch ($RunningDate.Day){
-            {1..7 -contains $_} { $WeekNumber = $WeekLabel -f "1" }
-            {8..15 -contains $_} { $WeekNumber = $WeekLabel -f "2" }
-            {16..23 -contains $_} { $WeekNumber = $WeekLabel -f "3" }
-            {24..31 -contains $_} { $WeekNumber = $WeekLabel -f "4" }
-            }
-        $WeekFolder = Get-Item -Path $(Join-Path $script:RunningFolder $WeekNumber) -ErrorAction SilentlyContinue
-        if (-not $WeekFolder) { $script:RunningFolder = New-Item -Path $(Join-Path $script:RunningFolder $WeekNumber) -ItemType Directory }
-        else { $script:RunningFolder = $WeekFolder }
-        }
-    if ($Days.IsPresent) { $script:RunningFolder = New-DateFolder -RunningDate $RunningDate -Path $script:RunningFolder -DateInterval "Day" -DateFormat $DayFormat }
-    if ($Hours.IsPresent) { 0..24 | ForEach-Object { New-Item -Path $script:RunningFolder -Name $($HourLabel -f $("$($_)u00")) -ItemType Directory | out-Null }}
-    #(re)set running values
+foreach ($RootFolder in $RootFolders){
+    [datetime]$RunningDate = $StartDate
     $script:RunningFolder = $RootFolder
+    #total number of days = difference days + startday + endday
+    [int]$AlmanakTimeSpan = (New-TimeSpan -Start $StartDate -End $EndDate).Days + 2
+    [int]$NumberOfDaysProcessed = 0
+    for ($RunningDate = $StartDate; $RunningDate -le $EndDate; $Runningdate = $RunningDate.AddDays(1)) {
+        Write-Progress -Activity "New-Almanak" -Status "Creating almanak folder tree under $($RootFolder)" -PercentComplete ($NumberOfDaysProcessed++ / $AlmanakTimeSpan * 100)
+        if ($Years.IsPresent) { $script:RunningFolder = New-DateFolder -RunningDate $RunningDate -Path $script:RunningFolder -DateInterval "Year" -DateFormat $YearFormat }
+        if ($Quarters.IsPresent) {
+            switch ($RunningDate.Month){
+                {1..3 -contains $_} { $QuarterNumber = $QuarterLabel -f "1" }
+                {4..6 -contains $_} { $QuarterNumber = $QuarterLabel -f "2" }
+                {7..9 -contains $_} { $QuarterNumber = $QuarterLabel -f "3" }
+                {10..12 -contains $_} { $QuarterNumber = $QuarterLabel -f "4" }
+                }
+                $QuarterFolder = Get-Item -Path $(Join-Path $script:RunningFolder $QuarterNumber) -ErrorAction SilentlyContinue
+                if (-not $QuarterFolder) { $script:RunningFolder = New-Item -Path $(Join-Path $script:RunningFolder $QuarterNumber) -ItemType Directory }
+                else { $script:RunningFolder = $QuarterFolder }
+            }
+        if ($Months.IsPresent) { $script:RunningFolder = New-DateFolder -RunningDate $RunningDate -Path $script:RunningFolder -DateInterval "Month" -DateFormat $MonthFormat }
+        if ($Weeks.IsPresent) {
+            switch ($RunningDate.Day){
+                {1..7 -contains $_} { $WeekNumber = $WeekLabel -f "1" }
+                {8..15 -contains $_} { $WeekNumber = $WeekLabel -f "2" }
+                {16..23 -contains $_} { $WeekNumber = $WeekLabel -f "3" }
+                {24..31 -contains $_} { $WeekNumber = $WeekLabel -f "4" }
+                }
+            $WeekFolder = Get-Item -Path $(Join-Path $script:RunningFolder $WeekNumber) -ErrorAction SilentlyContinue
+            if (-not $WeekFolder) { $script:RunningFolder = New-Item -Path $(Join-Path $script:RunningFolder $WeekNumber) -ItemType Directory }
+            else { $script:RunningFolder = $WeekFolder }
+            }
+        if ($Days.IsPresent) { $script:RunningFolder = New-DateFolder -RunningDate $RunningDate -Path $script:RunningFolder -DateInterval "Day" -DateFormat $DayFormat }
+        #minutes depend on presence of hours because of granularity per day of for loop
+        if ($Hours.IsPresent) {
+            $script:HourFolder = ""
+            0..23 | ForEach-Object {
+                $script:HourFolder = Join-Path $script:RunningFolder $($HourLabel -f $("$($_)u00"))
+                if (-not (Get-Item $script:HourFolder -ErrorAction SilentlyContinue )) { $script:HourFolder = New-Item -Path $script:HourFolder -ItemType Directory -ErrorAction SilentlyContinue }
+                if ($Minutes.IsPresent) {
+                    0..59 | ForEach-Object {
+                        $MinuteFolder = Join-Path $script:HourFolder $($MinuteLabel -f $_).PadLeft(2,"0")
+                        if (-not ( Get-Item -Path $MinuteFolder -ErrorAction SilentlyContinue )){ New-Item -Path $MinuteFolder -ItemType Directory -ErrorAction SilentlyContinue | out-Null }
+                        }
+                    }
+                }
+            }
+        #(re)set running values
+        $script:RunningFolder = $RootFolder
+        }
     }
 }
 
 begin {
-[datetime]$RunningDate = $StartDate
-$script:RunningFolder = $RootFolder
+
 switch ($DateFormat){
     "FullNames"{
         $YearFormat = 'Year_%Y' 
@@ -62,6 +82,7 @@ switch ($DateFormat){
         $WeekLabel = "Week_{0}"
         $QuarterLabel = "Quarter_{0}"
         $HourLabel = "Hour_{0}"
+        $MinuteLabel = "Minute_{0}"
         }
     "Abbreviated"{
         $YearFormat = 'Y_%y'
@@ -70,6 +91,7 @@ switch ($DateFormat){
         $WeekLabel = "W_{0}"
         $QuarterLabel = "Q_{0}"
         $HourLabel = "H_{0}"
+        $MinuteLabel = "Min_{0}"
         }
     "Numeric"{
         $YearFormat = '%y'
@@ -78,6 +100,7 @@ switch ($DateFormat){
         $WeekLabel = "{0}"
         $QuarterLabel = "{0}"
         $HourLabel = "{0}"
+        $MinuteLabel = "{0}"
         }
     }
 
@@ -97,5 +120,5 @@ return $NewFolder
 }#begin
 
 end {
-Write-Verbose "created folders from $($StartDate) to $($EndDate) under $($RootFolder)"
+foreach ($RootFolder in $RootFolders){ Write-Verbose "created folders from $($StartDate) to $($EndDate) under $($RootFolder)" }
 }
